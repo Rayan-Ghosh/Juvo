615048cd34297f6623c67cb60b133b49f8fd2098
Rayan Ghosh <74374932+Rayan-Ghosh@users.noreply.github.com>
2025-09-13 13:41:07 +0530

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/voice-mood-detection.ts';
-import '@/ai/flows/personalized-affirmations.ts';
-import '@/ai/flows/chatbot-therapy.ts';
-import '@/ai/flows/wellness-check-in.ts';
