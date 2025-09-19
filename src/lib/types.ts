8b9a8147ce706571cbe51bd045cdc3444656f35c
Rayan Ghosh <74374932+Rayan-Ghosh@users.noreply.github.com>
2025-09-19 22:39:34 +0530

    Delete src directory

--- src/lib/types.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/types.ts
-
-export interface VitalLog {
-    id: string;
-    bp?: string; // Blood Pressure e.g. "120/80"
-    stress?: number;
-    spo2?: number; // Blood oxygen saturation
-    createdAt: Date;
-}
